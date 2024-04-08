7bb8333428fc8dc5d0f21b225c1c8f0905b09941
ROBIN Benoît <163303872+Lorddarcia@users.noreply.github.com>
2024-04-08 11:13:42 +0200

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    if (Math.floor(mySprite.y / 16) == 57 && Math.floor(mySprite.x / 16) - 9 == 1) {
+        if (game.ask("Voulez vous entrer?")) {
+            tiles.placeOnTile(mySprite, tiles.getTileLocation(9 - 1, 57))
+            mySprite2.setFlag(SpriteFlag.Invisible, true)
+        }
+    } else if (Math.floor(mySprite.y / 16) == 57 && Math.floor(mySprite.x / 16) - 9 == -1) {
+        if (game.ask("Voulez vous Sortir?")) {
+            tiles.placeOnTile(mySprite, tiles.getTileLocation(9 + 1, 57))
+            mySprite2.setFlag(SpriteFlag.Invisible, false)
+        }
+    }
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.showLongText(Math.floor(mySprite.y / 16), DialogLayout.Bottom)
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`niveau2`)
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . 2 2 2 2 2 2 . . . . . 
     . . . . 2 2 2 2 2 2 2 2 2 2 . . 
     . . . . e e e e 3 3 e 3 . . . . 
@@ -20,10 +35,10 @@ let mySprite = sprites.create(img`
     . . . e e e e . . . e e e e . . 
     . . e e e e e . . . e e e e e . 
     `, SpriteKind.Player)
-let mySprite2 = sprites.create(assets.image`SpriteToit`, SpriteKind.Player)
+mySprite2 = sprites.create(assets.image`SpriteToit`, SpriteKind.Player)
 mySprite2.setPosition(80, 816)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 55))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(11, 55))
 controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
-mySprite2.z += 1
-mySprite.z += 2
+mySprite.setFlag(SpriteFlag.ShowPhysics, true)
+mySprite2.setFlag(SpriteFlag.Invisible, false)
