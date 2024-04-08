9d6555bb89df44563bfec6aeab13033d172428a8
ROBIN Benoît <163303872+Lorddarcia@users.noreply.github.com>
2024-04-08 11:58:12 +0200

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+function placerSpriteToit (ArrCoin: tiles.Location[], mySprite: Sprite) {
+    CoinSupDrt = (ArrCoin[2].column + 1) * 16
+    CoinSupDrt2 = ArrCoin[2].row * 16
+    CoinSupGch = ArrCoin[0].column * 16
+    CoinInfDrt = (ArrCoin[3].row + 1) * 16
+    mySprite.setPosition(CoinSupGch + (CoinSupDrt - CoinSupGch) / 2, CoinSupDrt2 + (CoinInfDrt - CoinSupDrt2) / 2)
+}
+function CréerSpriteToit (ArrCoin: tiles.Location[]) {
+    CoinSupDrt = (ArrCoin[2].column + 1) * 16
+    CoinSupDrt2 = ArrCoin[2].row * 16
+    CoinSupGch = ArrCoin[0].column * 16
+    CoinInfDrt = (ArrCoin[3].row + 1) * 16
+    ImgToit = image.create(CoinSupDrt - CoinSupGch, CoinInfDrt - CoinSupDrt2)
+    ImgToit.fill(4)
+    return ImgToit
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Math.floor(mySprite.y / 16) == 57 && Math.floor(mySprite.x / 16) - 9 == 1) {
         if (game.ask("Voulez vous entrer?")) {
@@ -12,10 +28,25 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    game.showLongText(Math.floor(mySprite.y / 16), DialogLayout.Bottom)
+    if (debug == 0) {
+        mySprite3.setFlag(SpriteFlag.Invisible, true)
+        debug = 1
+    } else {
+        mySprite3.setFlag(SpriteFlag.Invisible, false)
+        debug = 0
+    }
 })
+let ImgToit: Image = null
+let CoinInfDrt = 0
+let CoinSupGch = 0
+let CoinSupDrt2 = 0
+let CoinSupDrt = 0
+let mySprite3: Sprite = null
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
+let debug = 0
+debug = 0
+let ArrCoin: tiles.Location[] = []
 tiles.setCurrentTilemap(tilemap`niveau2`)
 mySprite = sprites.create(img`
     . . . . . 2 2 2 2 2 2 . . . . . 
@@ -42,3 +73,8 @@ controller.moveSprite(mySprite, 100, 100)
 scene.cameraFollowSprite(mySprite)
 mySprite.setFlag(SpriteFlag.ShowPhysics, true)
 mySprite2.setFlag(SpriteFlag.Invisible, false)
+for (let valeur of tiles.getTilesByType(assets.tile`TuileCoinBatTest`)) {
+    ArrCoin.push(valeur)
+}
+mySprite3 = sprites.create(CréerSpriteToit(ArrCoin), SpriteKind.Player)
+placerSpriteToit(ArrCoin, mySprite3)
